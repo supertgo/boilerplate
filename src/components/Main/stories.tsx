6f3ba4e2b3800646cb3b9ad99093396287c9f80a
supertgo <thiagoroberto2003@gmail.com>
2021-07-11 09:09:30 -0300

    Updating files with Dependabot and GitHub actions

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,11 +1,11 @@
-import { withKnobs, text } from '@storybook/addon-knobs'
-import Main from '.'
+import { withKnobs, text } from '@storybook/addon-knobs';
+import Main from '.';
 
 export default {
   title: 'Main',
   component: Main,
   decorators: [withKnobs]
-}
+};
 
 export const Basic = () => (
   <Main
@@ -15,4 +15,4 @@ export const Basic = () => (
       'TypeScript, ReactJS, NextJS e Styled Components'
     )}
   />
-)
+);
